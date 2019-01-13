b988758237c7731b9bfda4cccb68b92ff90df388
griase94 <andy.griesbeck@gmail.com>
2019-01-13 18:45:16 +0100

    storage impl

--- src/storage.rs
+++ src/storage.rs
@@ -2,5 +2,23 @@ use std::collections::HashMap;
 
 
 pub struct Storage<'a> {
-        pub data: HashMap<&'a str, &'a str>
+    data: HashMap<&'a str, &'a str>
+}
+
+impl Storage {
+    pub fn new() -> Storage {
+        return Storage { data: HashMap::new() };
+    }
+
+    fn put(key: str, value: str) {
+        data.insert(key, value);
+    }
+
+    fn get(key: str) -> Option<&str> {
+        return data.get(key);
+    }
+
+    fn delete(key: str){
+        remove(key);
+    }
 }
