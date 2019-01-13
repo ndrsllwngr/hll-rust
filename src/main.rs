45d69821fd4a07ccd160c0d24bd0585066fb5d3c
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2019-01-13 19:59:31 +0100

    Add network crate tokio and example echo server

--- src/main.rs
+++ src/main.rs
@@ -9,15 +9,12 @@ use std::collections::HashMap;
 use num_bigint::{BigInt, Sign};
 use std::str;
 
-use tokio::prelude::*;
-use tokio::io::copy;
-use tokio::net::TcpListener;
-
 mod node;
 mod storage;
 mod finger;
 mod constants;
 mod util;
+mod network;
 
 fn main() {
     println!("Hello, world!");
@@ -39,37 +36,8 @@ fn main() {
     */
 
     test_endian("test");
-
-    // // Bind the server's socket.
-    // let addr = "127.0.0.1:12345".parse().unwrap();
-    // let listener = TcpListener::bind(&addr)
-    //     .expect("unable to bind TCP listener");
-
-    // // Pull out a stream of sockets for incoming connections
-    // let server = listener.incoming()
-    //     .map_err(|e| eprintln!("accept failed = {:?}", e))
-    //     .for_each(|sock| {
-    //         // Split up the reading and writing parts of the
-    //         // socket.
-    //         let (reader, writer) = sock.split();
-
-    //         // A future that echos the data and returns how
-    //         // many bytes were copied...
-    //         let bytes_copied = copy(reader, writer);
-
-    //         // ... after which we'll print what happened.
-    //         let handle_conn = bytes_copied.map(|amt| {
-    //             println!("wrote {:?} bytes", amt)
-    //         }).map_err(|err| {
-    //             eprintln!("IO error {:?}", err)
-    //         });
-
-    //         // Spawn the future as a concurrent task.
-    //         tokio::spawn(handle_conn)
-    //     });
-
-    // // Start the Tokio runtime
-    // tokio::run(server);
+    network::start_server();
+    
 }
 
 fn test_endian(str: &str) {
