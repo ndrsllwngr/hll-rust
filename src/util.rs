dbe1ec26211b4d899202a0f58867dbc2f9eccbcf
griase94 <andy.griesbeck@gmail.com>
2019-01-13 19:46:29 +0100

    in range functions

--- src/util.rs
+++ src/util.rs
@@ -1,5 +1,7 @@
-use super::crypto::digest::Digest;
-use super::crypto::sha1::Sha1;
+use crypto::digest::Digest;
+use crypto::sha1::Sha1;
+use num_bigint::BigInt;
+
 
 pub fn create_hash(string: &str) -> String {
 
@@ -13,4 +15,27 @@ pub fn create_hash(string: &str) -> String {
 // read hash digest
     let hex = hasher.result_str();
     return hex;
+}
+
+/**
+* Testing if key ∈ (n, successor]
+*/
+pub fn isInHalfRange(key: BigInt, n: BigInt, successor: BigInt) -> Bool {
+    if n < successor {
+        return (key > n && key <= successor) || (n == successor);
+    } else {
+        return (key > successor && key <= n) || (n == successor);
+    }
+}
+
+/**
+* Testing if key ∈ (left, right)
+*/
+//TODO check if left & right naming is reasonable
+pub fn isInRange(key: BigInt, left: BigInt, right: BigInt) -> Bool {
+    if left < right {
+        return (key > left && key < right) || (left == right && key != left);
+    } else {
+        return (key > right && key < left) || (left == right && key != left);
+    }
 }
