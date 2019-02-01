5dbb988487e6d8dc3b71d7a8af9482374ba3edc9
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2019-02-01 21:55:16 +0100

    Add shell script, which spawns 4 nodes
    Execute by typing `sh test.sh` in the project folder
    Terminate the script by pressing `CTRL+C`

--- test.sh
+++ test.sh
@@ -7,6 +7,10 @@ PROJECT_BUILD="/target/debug/hll-rust"
 #trap ctrl_c SIGINT SIGTERM EXIT
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nohup .$PROJECT_BUILD -i 10.0.1.2 -p 22222 > /dev/null 2>&1 &
-.$PROJECT_BUILD -i 10.0.1.2 -p 33333 -j 10.0.1.2:22222 && fg
-
+nohup   .$PROJECT_BUILD -i 10.0.1.2 -p 11111                   > /dev/null 2>&1 &
+sleep 1
+nohup   .$PROJECT_BUILD -i 10.0.1.2 -p 22222 -j 10.0.1.2:11111 > /dev/null 2>&1 &
+sleep 1
+nohup   .$PROJECT_BUILD -i 10.0.1.2 -p 33333 -j 10.0.1.2:11111 > /dev/null 2>&1 &
+sleep 1
+        .$PROJECT_BUILD -i 10.0.1.2 -p 44444 -j 10.0.1.2:11111 && fg
