ed55bd838a306099ea96ba754ee4f299e336ca79
griase94 <andy.griesbeck@gmail.com>
2019-02-04 16:59:06 +0100

    improved shell script

--- test_macOS.sh
+++ test_macOS.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
+
+#
+# run this as follows:
+# sh ./test_macOS.sh IP_ADDRESS NUMBER_OF_NODES
+#
+
 PROJECT_BUILD="/target/debug/hll-rust"
-TEST_IP="145.97.252.154"
+TEST_IP=$1
+NUMBER_OF_NODES=$2
 
 cargo build
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nohup   .$PROJECT_BUILD -i $TEST_IP -p 10000                  > /dev/null 2>&1 &
-sleep .5
-nohup   .$PROJECT_BUILD -i $TEST_IP -p 10010 -j $TEST_IP:10000 > /dev/null 2>&1 &
-sleep .5
-nohup   .$PROJECT_BUILD -i $TEST_IP -p 10020 -j $TEST_IP:10000 > /dev/null 2>&1 &
-sleep .5
-nohup   .$PROJECT_BUILD -i $TEST_IP -p 10030 -j $TEST_IP:10000 > /dev/null 2>&1 &
-sleep .5
-nohup   .$PROJECT_BUILD -i $TEST_IP -p 10040 -j $TEST_IP:10000 > /dev/null 2>&1 &
-sleep .5
-nohup   .$PROJECT_BUILD -i $TEST_IP -p 10050 -j $TEST_IP:10000 > /dev/null 2>&1 &
-sleep .5
-nohup   .$PROJECT_BUILD -i $TEST_IP -p 10060 -j $TEST_IP:10000 > /dev/null 2>&1 &
-sleep .5
-        .$PROJECT_BUILD -i $TEST_IP -p 10070 -j $TEST_IP:10000 && fg
+nohup   .${PROJECT_BUILD} -i ${TEST_IP} -p 10001 > /dev/null 2>&1 &
+for i in `seq 2 ${NUMBER_OF_NODES}`
+do
+    let port = 1
+    sleep .5
+    if (($i == $NUMBER_OF_NODES))
+    then
+        .${PROJECT_BUILD} -i ${TEST_IP} -p $((10000+$i)) -j ${TEST_IP}:10001 && fg
+    else
+        nohup   .${PROJECT_BUILD} -i ${TEST_IP} -p $((10000+$i)) -j ${TEST_IP}:10001 > /dev/null 2>&1 &
+    fi
+done
+
