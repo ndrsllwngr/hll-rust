751a1c701caec899e1dad03f22c3031f2b5be080
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2019-02-07 23:07:16 +0100

    Update test scripts

--- test_macOS.sh
+++ test_macOS.sh
@@ -11,15 +11,15 @@ NUMBER_OF_NODES=$2
 cargo build
 trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
-nohup   .${PROJECT_BUILD} -i ${TEST_IP} -p 10001 > /dev/null 2>&1 &
+nohup   .${PROJECT_BUILD} ${TEST_IP} 10001 > /dev/null 2>&1 &
 for i in `seq 2 ${NUMBER_OF_NODES}`
 do
     sleep .5
     if (($i == $NUMBER_OF_NODES))
     then
-        .${PROJECT_BUILD} -i ${TEST_IP} -p $((10000+$i)) -j ${TEST_IP}:10001 && fg
+        .${PROJECT_BUILD} ${TEST_IP} $((10000+$i)) ${TEST_IP}:10001 && fg
     else
-        nohup   .${PROJECT_BUILD} -i ${TEST_IP} -p $((10000+$i)) -j ${TEST_IP}:10001 > /dev/null 2>&1 &
+        nohup   .${PROJECT_BUILD} ${TEST_IP} $((10000+$i)) ${TEST_IP}:10001 > /dev/null 2>&1 &
     fi
 done
 
