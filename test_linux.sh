9ce23dabb35276283580ce459d73f24c1440419a
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2019-02-08 11:07:30 +0100

    Fix shell script

--- test_linux.sh
+++ test_linux.sh
@@ -4,13 +4,20 @@
 # run this as follows:
 # sh ./test_macOS.sh IP_ADDRESS NUMBER_OF_NODES
 #
-
 PROJECT_BUILD="/target/debug/hll-rust"
 TEST_IP=$1
 NUMBER_OF_NODES=$2
 
+trap 'quit' 2 15
+
+quit()
+{
+  echo "Caught SIGINT / SIGTERM ...exiting now."
+  exit 1
+}
+
+
 cargo build
-trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 
 nohup   .${PROJECT_BUILD} ${TEST_IP} 10001 > /dev/null 2>&1 &
 for i in `seq 2 ${NUMBER_OF_NODES}`
